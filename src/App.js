eb0eef224e7d214679e6526280f559563f7fece6
jinfeng <wjinfeng@yahoo.ca>
2022-08-11 22:04:05 -0400

    配置路由

--- src/App.js
+++ src/App.js
@@ -1,5 +1,23 @@
+import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom'
+import Home from './pages/Layout'
+import Login from './pages/Login'
+
 function App() {
-  return <div className="App">App root conponent</div>
+  return (
+    <Router>
+      <div className="App">
+        <Link to="/login">login</Link>
+        <Link to="/home">home</Link>
+      </div>
+
+      {/* 配置route 规则 'Switch are replaced by Routes in V6' */}
+      {/* Replace component with element in v6 */}
+      <Routes>
+        <Route path="/home" element={<Home />}></Route>
+        <Route path="/login" element={<Login />}></Route>
+      </Routes>
+    </Router>
+  )
 }
 
 export default App
